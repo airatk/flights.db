71255dd9e9b2a7911cbf06b277cf848f7228a212
airatk <kamairat@icloud.com>
2020-11-08 10:01:38 +0300

    Basic structure & styles were implemented

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask
+from flask import render_template
+
+
+app: Flask = Flask(import_name=__name__)
+
+
+@app.route("/")
+def show_base_page():
+    return render_template("base.html")
+
+
+@app.route("/airline", methods=[ "GET" ])
+def show_airline_page():
+    return render_template("airline.html")
+
+@app.route("/airline", methods=[ "POST" ])
+def response_to_form_on_airline_page():
+    # TODO: Implement fetching airlines data.
+    # TODO: Implement fetching planes data.
+    
+    airlines = [ i for i in range(2) ]
+    planes = [ i for i in range(2) ]
+    
+    return render_template("airline.html", airlines=airlines, planes=planes)
+
+
+@app.route("/airport", methods=[ "GET" ])
+def show_airport_page():
+    return render_template("airport.html")
+
+@app.route("/airport", methods=[ "POST" ])
+def response_to_form_on_airport_page():
+    # TODO: Implement fetching airports data.
+    # TODO: Implement fetching flights data.
+    
+    airports = [ i for i in range(2) ]
+    flights = [ i for i in range(2) ]
+    
+    return render_template("airport.html", airports=airports, flights=flights)
+
+
+@app.route("/client", methods=[ "GET" ])
+def show_client_page():
+    return render_template("client.html")
+
+@app.route("/client", methods=[ "POST" ])
+def response_to_form_on_client_page():
+    # TODO: Implement fetching clients data.
+    # TODO: Implement fetching tickets data.
+    
+    clients = [ i for i in range(2) ]
+    tickets = [ i for i in range(2) ]
+    
+    return render_template("client.html", clients=clients, tickets=tickets)
